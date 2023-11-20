3e3f64800bfe406d33706011772c493500d43adf
SelimImranov <selimimranov0@gmail.com>
2023-11-21 01:33:38 +0400

    lesson-1 test

---
+++ lesson3/backend118-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend118_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte age = 15;
+            //Console.WriteLine(age + 20);
+            //Console.WriteLine("zehmet olmasa adinizi daxil edin");
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("zehmet olmasa yasiniz daxil edin");
+            //byte age =Convert.ToByte (Console.ReadLine());
+
+            //Console.WriteLine(Name + "" +age);
+            Console.WriteLine("1 ci ededi daxil et");
+            byte x1=Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("2 ci ededi daxil et");
+            byte x2=Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("netice");
+            Console.WriteLine(x1 + x2);
+
+          
+            
+          
+        }
+    }
+}
