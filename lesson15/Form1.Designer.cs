f025a8da42a4aebe3c4c27249f854659d410c75f
SelimImranov <selimimranov0@gmail.com>
2024-01-28 12:31:30 +0400

    lesson 15

---
+++ lesson15/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ww._118
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtname2 = new System.Windows.Forms.TextBox();
+            this.buttonadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(50, 54);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(192, 26);
+            this.txtname.TabIndex = 2;
+          
+            // 
+            // txtname2
+            // 
+            this.txtname2.Location = new System.Drawing.Point(50, 115);
+            this.txtname2.Name = "txtname2";
+            this.txtname2.Size = new System.Drawing.Size(192, 26);
+            this.txtname2.TabIndex = 3;
+            // 
+            // buttonadd
+            // 
+            this.buttonadd.Location = new System.Drawing.Point(37, 185);
+            this.buttonadd.Name = "buttonadd";
+            this.buttonadd.Size = new System.Drawing.Size(205, 53);
+            this.buttonadd.TabIndex = 4;
+            this.buttonadd.Text = "button1";
+            this.buttonadd.UseVisualStyleBackColor = true;
+            this.buttonadd.Click += new System.EventHandler(this.buttonadd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1392, 562);
+            this.Controls.Add(this.buttonadd);
+            this.Controls.Add(this.txtname2);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtname2;
+        private System.Windows.Forms.Button buttonadd;
+    }
+}
+
---
+++ lesson15/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ww._118
+{
+    public partial class Form1 : Form
+    {
+        List<student> list = new List<student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void buttonadd_Click(object sender, EventArgs e)
+        {
+            string name=txtname.Text;  
+            string surname=txtname2.Text;
+
+            student student = new student();
+            student.Name = name;
+            student.Surname = surname;
+            list.Add(student);
+            MessageBox.Show($"{name} student add");
+            txtname.Text = "";
+            txtname2.Text = "";
+
+
+        }
+    }
+}
